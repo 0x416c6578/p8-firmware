2e0e197310b56b863a7add9183373ef873291a78
0x416c6578 <14030169+0x416c6578@users.noreply.github.com>
2020-09-17 20:59:51 +0100

    test commit

--- p8-firmware/headers/display.h
+++ p8-firmware/headers/display.h
@@ -7,26 +7,6 @@
 #include "pinout.h"
 #include "utils.h"
 
-/* class Display {
- public:
-  static void init();
-  static void wake();
-  static void sleep();
-  static void drawFilledRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint16_t colour);
-  static void clear(bool appDrawClear = false);
-  static void drawChar(uint32_t x, uint32_t y, uint8_t pixelsPerPixel, char character, uint16_t colourFG, uint16_t colourBG);
-  static void drawString(uint32_t x, uint32_t y, uint8_t pixelsPerPixel, char* string, uint16_t colourFG = COLOUR_WHITE, uint16_t colourBG = COLOUR_BLACK);
-  static void drawString(uint32_t x, uint32_t y, uint8_t pixelsPerPixel, String string, uint16_t colourFG = COLOUR_WHITE, uint16_t colourBG = COLOUR_BLACK);
-  static void drawInt(uint32_t x, uint32_t y, uint8_t pixelsPerPixel, int toWrite, uint16_t colourFG = COLOUR_WHITE, uint16_t colourBG = COLOUR_BLACK, bool precedingZeroes = false);
-  static void drawUnfilledRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint8_t lineWidth, uint16_t colour);
-  static void drawUnfilledRect(uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint8_t lineWidth, uint16_t rectColour, char character, uint8_t fontSize);
-
- private:
-  static void setWriteWindow(uint32_t x, uint32_t y, uint32_t w, uint32_t h);
-  static void writeRectToDisplay(uint16_t colour);
-  static void drawCharPixelToBuffer(int charColumn, int charRow, uint8_t pixelsPerPixel, bool pixelInCharHere, uint16_t colourFG, uint16_t colourBG);
-}; */
-
 //Old C style function definitions
 void initDisplay();
 void wakeDisplay();
